eb69d64230886f2133d8f37be1ade42c5e7a6079
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-09-13 00:21:35 +0600

    login-page-done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,50 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
-    return (
-        <div>
-            <h3>
-                This is login
-            </h3>
+  return (
+    <div className='form-container'>
+      <div>
+        <h2 className='form-title'>
+          Login
+        </h2>
+        <Form>
+          <Form.Group className="input-group" >
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" />
+
+          </Form.Group>
+
+          <Form.Group className="input-group">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+
+          <Button className='submit-btn' type="submit">
+            Login
+          </Button>
+        </Form>
+        <p>New to Ema-John? <Link className='form-link' to="/signup">Create New Account</Link></p>
+
+        <div className='divider-part'>
+          <div className='divider'>
+            
+            
+          </div>
+         or
+          <div className='divider'></div>
         </div>
-    );
+
+          <button className='google-btn'>Continue With Google</button>
+
+        </div>
+        
+      </div>
+    
+    
+  );
 };
 
 export default Login;
